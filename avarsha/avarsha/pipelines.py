6f3e94361bbfc768fab9cd614910886d34082deb
revotu <632624460@qq.com>
2016-05-11 17:51:07 +0800

    bug fixed dynamic set domains_url

--- avarsha/avarsha/pipelines.py
+++ avarsha/avarsha/pipelines.py
@@ -162,9 +162,9 @@ class AvarshaPipeline(object):
 
         if spider.settings['VERSION'] == 'DEV':
             # for test for spider
-            start_urls = ['http://jinzabridal.com']
-            feeder.init_test_feeds(start_urls)
+            start_urls = spider.start_urls
             spider.allowed_domains.append(start_urls[0][len("http://"):])
+            feeder.init_test_feeds(start_urls)
         else:
             feeder.init_feeds(spider_name=spider.name,
                 feed_type=spider.feed_type)
@@ -201,6 +201,16 @@ class AvarshaPipeline(object):
         data.append(item['product_id'])
         ws.append(data)
         wb.save('D:/www/dev-web-crawler/terms-products.xlsx')
+
+    def storeEmail(self ,url , email):
+        wb = load_workbook('D:/www/dev-web-crawler/B2B-Lists-2016-05-10.xlsx')
+        ws = wb.active
+        email = '\t'.join(email)
+        for i in range(1,326):
+            if ws.cell(row = i,column = 1).value == url:
+                ws.cell(row = i,column = 2).value = email
+                break
+        wb.save('D:/www/dev-web-crawler/B2B-Lists-2016-05-10.xlsx')
         
     
     def store_to_excel(self , item):
